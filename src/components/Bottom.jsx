b532a852467451616bc0e7927c51c2fb9480502e
Brown <preciousebere090@gmail.com>
2023-03-30 09:53:33 +0200

    updates

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -9,7 +9,7 @@ const Bottom = () => {
             id:1,
             child: (
                 <>
-                  <FaLinkedin style={{backgroundColor : "transparent", color: "rgb(0, 119, 181)"}} size={33} />
+                  <FaLinkedin style={{backgroundColor : "transparent", color: "rgb(0, 119, 181)"}} size={25} />
                 </>
             ),
             href: "https://ng.linkedin.com/in/precious-ebere-838a68263"
@@ -18,43 +18,43 @@ const Bottom = () => {
             id:2,
             child: (
                 <>
-                     <FaGithub style={{backgroundColor : "transparent", color: "white"}} size={33} />
+                     <FaGithub style={{backgroundColor : "transparent", color: "white"}} size={25} />
                 </>
             ),
             href: "https://github.com/preshbrown"
         },
+        // {
+        //     id:3,
+        //     child: (
+        //         <>
+        //              <FaTwitter style={{backgroundColor : "transparent", color: "rgb(29, 161, 242)"}} size={25} />
+        //         </>
+        //     ),
+        //     href: ""
+        // },
         {
             id:3,
             child: (
                 <>
-                     <FaTwitter style={{backgroundColor : "transparent", color: "rgb(29, 161, 242)"}} size={33} />
-                </>
-            ),
-            href: ""
-        },
-        {
-            id:4,
-            child: (
-                <>
-                     <HiOutlineMail style={{backgroundColor : "transparent", color: "red"}} size={33} />
+                     <HiOutlineMail style={{backgroundColor : "transparent", color: "red"}} size={25} />
                 </>
             ),
             href: "mailto:preciousebere090@gmail.com"
         },
         {
-            id:5,
+            id:4,
             child: (
                 <>
-                     <FaFacebook style={{backgroundColor : "transparent", color: "rgb(66, 103, 178)"}} size={33} />
+                     <FaFacebook style={{backgroundColor : "transparent", color: "rgb(66, 103, 178)"}} size={25} />
                 </>
             ),
             href: "https://www.facebook.com/precious.brown.98892?mibextid=ZbWKwL"
         },
         {
-            id:6,
+            id:5,
             child: (
                 <>
-                     <BsFillPersonLinesFill style={{ color: "white"}}  size={33} />
+                     <BsFillPersonLinesFill style={{ color: "white"}}  size={25} />
                 </>
             ),
             href: "/resume.pdf",
@@ -64,12 +64,11 @@ const Bottom = () => {
     ]
 
 
-
   return (
     <div className="bg-gradient-to-b from-gray-800 to-black w-full h-auto ">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4  ">
 
-          <ul className=" flex flex-row h-auto mt-10 lg:hidden">
+          <ul className="w-full flex flex-row h-auto mt-10 lg:hidden">
             {links.map(link => (
                 <li key={link.id} className={"flex justify-between items-center p-0 m-5  "  }>
                 <a 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div name="home"
-       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+       className="h-auto pt-15 w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4  md:flex-row">
             <div className="flex flex-col justify-center h-full md:w-1/2">
                 <h2 className="text-4xl sm:text-5xl font-bold text-white ">
