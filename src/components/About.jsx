c475246f80b06420b332cc3b21b99a9357a57a05
Brown <preciousebere090@gmail.com>
2023-03-30 15:26:44 +0200

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
        name="about"
-       className="pt-16 pb-5 w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white ">
+       className="pt-17 pb-5 w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white ">
      <div className=" max-w-screen-lg px-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-4">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">
@@ -12,7 +12,7 @@ const About = () => {
             </p>
         </div>
 
-        <p className="text-xl mt-10">
+        <p className="text-xl mt-8">
             I'm a FULL STACK WEB DEVELOPER! I work at the intersection of curiosity, reasoning and imagination, which guide me to uncover insights, creating engaging, optimized and stand out web applications. Building interactive CLIENT and SERVER-SIDE web applications has always been a longstanding ambition of mine, am adept at handling muiltiple projects in a fast paced environment. Excellent at time management, organization, problem solving and meeting dead lines.<br />
             As for code; I like to dig in with JAVASCRIPT, Animations (React-Transition-Group), and nerd out in REACT. 
         </p>
